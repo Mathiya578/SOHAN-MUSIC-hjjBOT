c08bfafabb1787d7fa02b7e24fb8afc1664ac2cc
Sohan Matheesha <105502291+Mathiya578@users.noreply.github.com>
2022-09-01 22:24:41 +0530

    🥀Sohan Matheesha 😺

---
+++ story.js
@@ -0,0 +1,22 @@
+const { story, bot } = require('../lib/')
+
+bot(
+	{
+		pattern: 'story ?(.*)',
+		fromMe: true,
+		desc: 'Download Instagram stories',
+		type: 'download',
+	},
+	async (message, match) => {
+		match = match || message.reply_message.text
+		if (!match) return await message.send('_Example : story username_')
+		const result = await story(match)
+		if (!result.length)
+			return await message.send('*Not found*', {
+				quoted: message.quoted,
+			})
+		for (const url of result) {
+			await message.sendFromUrl(url)
+		}
+	}
+) 
