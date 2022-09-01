044bc9e7b15a3b6e09ea8c888523566417cfeadb
Sohan Matheesha <105502291+Mathiya578@users.noreply.github.com>
2022-09-01 21:23:15 +0530

    🥀Sohan Matheesha 😺

---
+++ plugins/antifake.js
@@ -0,0 +1,49 @@
+const { bot, genHydratedButtons, antiList, enableAntiFake } = require('../lib/')
+
+bot(
+	{
+		pattern: 'antifake ?(.*)',
+		fromMe: true,
+		desc: 'set antifake',
+		type: 'group',
+		onlyGroup: true,
+	},
+	async (message, match) => {
+		if (!match)
+			return await message.send(
+				await genHydratedButtons(
+					[
+						{
+							urlButton: {
+								text: 'Example',
+								url: 'https://github.com/lyfe00011/whatsapp-bot-md/wiki/antifake',
+							},
+						},
+						{ button: { id: 'antifake list', text: 'LIST' } },
+						{ button: { id: 'antifake on', text: 'ON' } },
+						{ button: { id: 'antifake off', text: 'OFF' } },
+					],
+					'Antifake'
+				),
+				{},
+				'template'
+			)
+		if (match == 'list') {
+			let list = ''
+			let codes = await antiList(message.jid, 'fake')
+			await message.send(codes.join(','))
+			codes.forEach((code, i) => {
+				list += `${i + 1}. ${code}\n`
+			})
+			return await message.send('```' + list + '```')
+		}
+		if (match == 'on' || match == 'off') {
+			await enableAntiFake(message.jid, match)
+			return await message.send(
+				`_Antifake ${match == 'on' ? 'Activated' : 'Deactivated'}_`
+			)
+		}
+		await enableAntiFake(message.jid, match)
+		return await message.send('_Antifake Updated_')
+	}
+) 
