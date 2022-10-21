e7aef59980f084f9f353a193d9ef96e966d152be
Sohan Matheesha <105502291+Mathiya578@users.noreply.github.com>
2022-10-21 15:10:55 +0530

    Sohan Matheesha

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
-global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
+global.vidmenu = fs.readFileSync("./CyberMedia/theme/Cheems-bot.mp4") //gif and video menu
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -54,29 +54,29 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['94757962326'] //ur owner number
+global.ownername = "Sohan Matheesha " //ur owner name
+global.ytname = "YT: sohan" //ur yt chanel name
+global.socialm = "GitHub: Mathiya578" //ur github or insta name
+global.location = "Srilanka, Gampha District, Ganemulla" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['94757962326']
+global.ownertag = '94757962326' //ur tag number
+global.botname = 'CYBER-MAFIA-MUSIC-BOT' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.botscript = 'https://github.com/Mathiya578/CYBER-MAFIA-MUSIC-BOT' //script link
+global.reactmoji = "❤️" //ur menu react emoji
+global.themeemoji = "🥰" //ur theme emoji
+global.packname = "Sohan Matheesha Stickers🙈♥️" //ur sticker watermark packname
+global.author = "Sohan Matheesha" //ur sticker watermark author
 
 //Bot theme media
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.thum = fs.readFileSync("./CyberMedia/theme/mafia.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./CyberMedia/theme/mafia.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./CyberMedia/theme/mafia.jpg") //ur error pic
+global.thumb = fs.readFileSync("./CyberMedia/theme/mafia.jpg") //ur thumb pic
 
 //damtabase
 global.premium = ['916909137213'] //ur premium numbers
@@ -102,7 +102,7 @@ global.ntilink = []
 //omther
 global.sessionName = 'session'
 global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','🥰','❤️','😺']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
